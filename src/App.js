e29ec30355a64c6c599f7cd2af15af6dddbaa39d
Sanchit-Trikha <sanchittrikha43@gmail.com>
2022-08-09 13:00:11 +0530

    logo changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -31,20 +31,7 @@ function App() {
         <input type="checkbox" className='input3'></input>
         <span>&nbsp;&nbsp;&nbsp; stirpe.jennifer@gmail.com</span>
       </div>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+   
     </div>
   );
 }
